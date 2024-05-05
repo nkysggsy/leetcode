5fee1a5d6fca60bbd9e62f208660605d2ede7898
nk_ysg <nk_ysg@163.com>
2024-05-05 19:09:28 +0800

    53_maxSubArray.cpp

---
+++ ds/1.1/53_maxSubArray.cpp
@@ -0,0 +1,17 @@
+//
+// Created by yesonggao on 2024/5/5.
+//
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> f(n + 1);
+
+        int max_val = -1e4;
+        for (int i = 0; i < n; i++) {
+            f[i + 1] = max(nums[i], f[i] + nums[i]);
+            max_val = max(f[i + 1], max_val);
+        }
+        return max_val;
+    }
+};
